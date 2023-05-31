84736238c5ba7f46a35c8ca28c9253ed6da20d78
Av1s <germanlavrikov23@gmail.com>
2023-05-31 17:06:41 +0300

    Some additions to last commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def frontpage(request):
+    return render(request,'core/base.html',{'name':'German','hobbies':["Python","English","Sport"]})
+
 # Create your views here.
