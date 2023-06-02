3d17e01de65c74a8c5aca15e13f448dccbc9dc85
Av1s <germanlavrikov23@gmail.com>
2023-06-03 00:06:41 +0300

    Add templates( FINALLY end of the 2-nd lesson)

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 def frontpage(request):
-    return render(request,'core/base.html',{'name':'German','hobbies':["Python","English","Sport"]})
+    articles = [
+        {'id': 1, 'tittle': 'First article', 'content':'This is a first article'},
+        {'id': 2, 'tittle': 'Second article', 'content':'This is a second article'},
+        {'id': 3, 'tittle': 'Third article', 'content':'This is a third article'},
+        {'id': 1, 'tittle': 'Fourth article', 'content':'This is a fourth article'},
+        {'id': 2, 'tittle': 'Fifth article', 'content':'This is a fifth article'},
+        {'id': 3, 'tittle': 'Sixth article', 'content':'This is a sixth article'}
+    ]
+    return render(request,'core/frontpage.html',{'articles': articles})
 
 # Create your views here.
