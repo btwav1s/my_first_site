7b5a4054ab81ad6583814f460b5d114de4a679d0
Av1s <germanlavrikov23@gmail.com>
2023-06-03 09:25:32 +0300

    Some last fixes to 2 lesson

--- core/views.py
+++ core/views.py
@@ -5,9 +5,9 @@ def frontpage(request):
         {'id': 1, 'tittle': 'First article', 'content':'This is a first article'},
         {'id': 2, 'tittle': 'Second article', 'content':'This is a second article'},
         {'id': 3, 'tittle': 'Third article', 'content':'This is a third article'},
-        {'id': 1, 'tittle': 'Fourth article', 'content':'This is a fourth article'},
-        {'id': 2, 'tittle': 'Fifth article', 'content':'This is a fifth article'},
-        {'id': 3, 'tittle': 'Sixth article', 'content':'This is a sixth article'}
+        {'id': 4, 'tittle': 'Fourth article', 'content':'This is a fourth article'},
+        {'id': 5, 'tittle': 'Fifth article', 'content':'This is a fifth article'},
+        {'id': 6, 'tittle': 'Sixth article', 'content':'This is a sixth article'}
     ]
     return render(request,'core/frontpage.html',{'articles': articles})
 
