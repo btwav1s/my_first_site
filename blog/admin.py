dc4f74c2d7ccd16f679c5f913c6878dee378066d
Av1s <germanlavrikov23@gmail.com>
2023-06-19 22:48:24 +0300

    Middle of the 4th lesson =)

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 # Register your models here.
+from .models import Article, Category, Tag
 
-from .models import Article, Category
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title__iexact', 'content_preview', 'category__name']
+    list_display = ['title','category','created_at',"updated_at"]
+
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Category)
+admin.site.register(Tag)
+
--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,11 @@ class Article(models.Model):
     slug = models.SlugField(verbose_name='URL')
     content_preview = models.TextField(verbose_name='Превью статьи')
     content = models.TextField(verbose_name='Текст статьи')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='articles', default=1, verbose_name='Категория')
+    tags = models.ManyToManyField('Tag', related_name="articles", verbose_name= "Теги", null=True)
     picture = models.TextField(verbose_name='Картинка')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата написания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата оновлення')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return self.title
@@ -16,13 +18,26 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
+        ordering = ['-created_at']
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название тега')
+    name = models.CharField(max_length=255, verbose_name='Название категории')
     slug = models.SlugField(default='', verbose_name='URL')
+
     def __str__(self):
         return self.name
     
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название тега')
+    slug = models.SlugField(default='', verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
