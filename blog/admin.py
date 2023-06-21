78db74810e2e8b716a4122f07187efe7f78b4764
Av1s <germanlavrikov23@gmail.com>
2023-06-21 12:40:23 +0300

    add tags and model ;)(some changes to last commit)

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,14 @@ from .models import Article, Category, Tag
 class ArticleAdmin(admin.ModelAdmin):
     search_fields = ['title__iexact', 'content_preview', 'category__name']
     list_display = ['title','category','created_at',"updated_at"]
+    list_filter = ['tags']
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name',"slug"]
+    list_filter = ['name']
 
 admin.site.register(Article,ArticleAdmin)
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Tag)
 
